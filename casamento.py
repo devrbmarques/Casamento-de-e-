73e6530d4a99f0a278ad918bede3562a91945267
devrbmarques <robson.tmec@gmail.com>
2017-04-27 00:14:44 -0400

    Iniciando a inserção dos casos de usos

--- casamento.py
+++ casamento.py
@@ -3,6 +3,8 @@ abertura_valida   = ['(', '[', '{']
 fechamento_valido = [')', ']', '}']
 
 def main(entrada):
+    x = trata_entrada(entrada)
+    print(x)
     percorrer_pilha()
 
 #remove todos os espaços da entrada
@@ -10,22 +12,25 @@ def trata_entrada(entrada):
     nova_entrada = ""
     for it in entrada:
         if it != ' ':
-            nova_entrada = it
-    verifica(nova_entrada)
+            nova_entrada += it
+    #passa por parâmetro a nova palavra sem os ' '.
+    return verifica(nova_entrada)
 
-#recebe palavra sem espaços
+#recebe palavra sem ' '
 def verifica(nova_entrada):
     for it in nova_entrada:
-        while abertura_valida <= abertura_valida[3]:
-            if it == abertura_valida:
-                print(it)
-            else:
-                print("Erro")
+        #verifica se o elemento 0 da entrada informada é um ')', ']' ou '}'.
+        #caso seja, False.
+        if it[0] in fechamento_valido:
+            return False
+
+
+
 
 def percorrer_pilha():
     while (len(pilha) != 0):
         print(pilha.pop())
 
 
-main(trata_entrada(input("")))
+main(input(""))
 # S->SS/(S)/[S]/{S}/()/[]/{}
